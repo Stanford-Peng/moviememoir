b79aaf721ae3d01cc6dbb680f045ed3198f557b5
Stanford <spen0022@student.monash.edu>
2020-05-10 00:19:16 +1000

    First commit

---
+++ app/src/main/java/com/example/moviememoir/signup.java
@@ -0,0 +1,25 @@
+package com.example.moviememoir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
